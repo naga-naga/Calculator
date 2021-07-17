8aaf50e76116d9481709374dadc114e49bf8b04a
naga-naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2021-07-17 19:45:37 +0900

    全体の大まかな処理を記述

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // define operator
+
+        // input
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+
+        // parse
+
+        // calculate
+
+        // output
+        System.out.println(inputString);
+
+        scanner.close();
+    }
+}
