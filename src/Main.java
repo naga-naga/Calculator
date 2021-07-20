36280186d754c8bbaa13e78bc80b388fba39c141
naga-naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2021-07-20 14:51:05 +0900

    逆ポーランド記法への変換の処理を追加．括弧には未対応

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // define operator
+        Map<String, Integer> priority = new HashMap<>();
+        priority.put("+", 10);
+        priority.put("-", 10);
+        priority.put("*", 20);
+        priority.put("/", 20);
 
         // input
         Scanner scanner = new Scanner(System.in);
         String inputString = scanner.nextLine();
 
         // parse
+        Parser parser = new Parser();
+        List<String> token = parser.tokenize(inputString);
+        System.out.println(token);
+
+        Deque<String> polish = parser.parse(inputString, priority);
+        System.out.println(polish);
 
         // calculate
 
--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,57 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+import java.util.Map;
+
 public class Parser {
-    public String[] tokenize(String str) {
+    public List<String> tokenize(String str) {
         // 記号の前後にスペースを入れ，連続するスペースを一つにし，分割する
-        return str.replaceAll("(\\W)", " $1 ").replaceAll("\\s+", " ").split("\\s");
+        String[] tokens = str.replaceAll("(\\W)", " $1 ").replaceAll("\\s+", " ").split("\\s");
+        List<String> list = new ArrayList<>(Arrays.asList(tokens));
+        list.add("END"); // 終わりを示す END トークンを追加しておく
+        return list;
+    }
+
+    public Deque<String> parse(String expressionString, Map<String, Integer> priority) {
+        Deque<String> reversePolishQueue = new ArrayDeque<>();
+        Deque<String> operatorStack = new ArrayDeque<>();
+        List<String> tokens = tokenize(expressionString);
+
+        for (String token : tokens) {
+            // 最後のトークンの場合
+            if (token.matches("END")) {
+                // 演算子スタックに残っている演算子をキューに出す
+                while (!operatorStack.isEmpty()) {
+                    reversePolishQueue.offerLast(operatorStack.pollFirst());
+                }
+            }
+
+            // トークンが数字の場合
+            if (token.matches("\\d+")) {
+                // キューにそのまま入れる
+                reversePolishQueue.offerLast(token);
+                continue;
+            }
+
+            // トークンが演算子の場合
+            // 演算子スタックが空ならスタックに積む
+            if (operatorStack.isEmpty()) {
+                operatorStack.offerFirst(token);
+                continue;
+            }
+
+            // 演算子スタックが空でない場合
+            // 現在のトークンがスタックの演算子より優先度が高いならスタックに積む
+            if (priority.get(token) > priority.get(operatorStack.peekFirst())) {
+                operatorStack.offerFirst(token);
+            } else { // そうでない場合はスタックの演算子をキューに出し，現在のトークンをスタックに積む
+                reversePolishQueue.offerLast(operatorStack.pollFirst());
+                operatorStack.offerFirst(token);
+            }
+        }
+
+        return reversePolishQueue;
     }
 }
